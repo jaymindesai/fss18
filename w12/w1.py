67b20a2bd8100db2ff9231f717891235b0e8b324
jaymindesai <jaymindesai766@gmail.com>
2018-08-24 21:02:57 -0400

    W1 Assignment.

--- w12/w1.py
+++ w12/w1.py
@@ -105,7 +105,7 @@ def testing_page11():
 
     assert sum(test_list) == 15
     assert test_list[-2] == 4
-    assert test_list[2:4] == [3,4]
+    assert test_list[2:4] == [3, 4]
 
 
 @O.k
@@ -119,6 +119,100 @@ def testing_page12():
 @O.k
 def testing_page13():
     """Tuples"""
+
+    def return_multiple_values():
+        return 1, 5
+
+    test_tuple = return_multiple_values()
+    assert test_tuple[1] == 5
+
+
+@O.k
+def testing_page14():
+    """Dictionaries"""
+
+    capitals = {
+        "India": "New Delhi",
+        "China": "Beijing",
+        "USA": "Washington, D.C.",
+        "France": "Paris",
+        "South Africa": ["Capetown", "Bloemfontein", "Pretoria"]
+    }
+
+    assert str("India" in capitals) == "True"
+    assert capitals["South Africa"][2] == "Pretoria"
+    assert capitals.get("Japan", "Not Found") == "Not Found"
+    assert len(capitals) == 5
+    assert str("Brazil" in capitals.keys()) == "False"
+
+
+@O.k
+def testing_page15():
+    """Default Dictionaries"""
+
+    from collections import defaultdict
+
+    def is_default(dictionary):
+        try:
+            dictionary["Num1"] += 1
+            return "True"
+        except KeyError:
+            return "False"
+
+    test_dict = {}
+    test_default_dict = defaultdict(int)
+
+    assert is_default(test_default_dict) == "True"
+    assert is_default(test_dict) == "False"
+
+
+@O.k
+def testing_page16():
+    """Counters"""
+
+    from collections import Counter
+
+    test_counter = Counter(["Hi", "Hello", "Howdy", "Hola", "Hi"]).most_common(1)
+    assert test_counter == [("Hi", 2)]
+
+
+@O.k
+def testing_page17():
+    """Sets"""
+
+    some_list = [1, 1, 2, 2, 3, 3, 4, 4]
+    test_set = set(some_list)
+
+    assert len(some_list) == 8
+    assert len(test_set) == 4
+
+@O.k
+def testing_page17_again():
+    """Membership Test"""
+
+    from time import time_ns as get_time
+
+    some_string = """We’ll use sets for two main reasons. The first is that in is a very fast operation on sets. If
+        we have a large collection of items that we want to use for a membership test, a set is more appropriate than a 
+        list. We’ll use sets for two main reasons. The first is that in is a very fast operation on sets. If we have a 
+        large collection of items that we want to use for a membership test, a set is more appropriate than a list. We’ll 
+        use sets for two main reasons. The first is that in is a very fast operation on sets. If we have a large collection 
+        of items that we want to use for a membership test, a set is more appropriate than a list. We’ll use sets for two 
+        main reasons. The first is that in is a very fast operation on sets. If we have a large collection of items that we 
+        want to use for a membership test, a set is more appropriate than a list"""
+
+    test_list = some_string.split()
+    test_set = set(test_list)
+
+    start_time_list = get_time()
+    "items" in test_list
+    time_taken_list = get_time() - start_time_list
+
+    start_time_set = get_time()
+    "items" in test_set
+    time_taken_set = get_time() - start_time_set
+
+    assert time_taken_set <= time_taken_list
 
 
 
